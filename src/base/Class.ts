0183e3a9ead27b4fdf8e5552363c2384a4525076
zhumxiang <zmx.jlife@gmail.com>
2020-10-29 13:43:17 +0800

    增加Class判断函数

--- src/base/Class.ts
+++ src/base/Class.ts
@@ -29,4 +29,7 @@ namespace cglib.base {
     export interface ClassP8<T, P1, P2, P3, P4, P5, P6, P7, P8> {
         new(p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7, p8: P8): T;
     }
+    export function isSameClass(cls1: Class<object>, cls2: Class<object>) {
+        return cls1 == cls2;
+    }
 }
