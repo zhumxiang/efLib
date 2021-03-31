9bb5025df817d162dc3dbaef09b56b2cedc5d31d
zhumxiang <zmx.jlife@gmail.com>
2021-03-31 15:56:37 +0800

    Class<T>添加prototype信息

--- src/common/Class.ts
+++ src/common/Class.ts
@@ -1,32 +1,35 @@
 namespace eflib {
-    export interface Class<T> {
+    export interface ClassBase<T> {
+        prototype: T;
+    }
+    export interface Class<T> extends ClassBase<T> {
         new(...params: any[]): T;
     }
-    export interface ClassP0<T> {
+    export interface ClassP0<T> extends ClassBase<T> {
         new(): T;
     }
-    export interface ClassP1<T, P1> {
+    export interface ClassP1<T, P1> extends ClassBase<T> {
         new(p1: P1): T;
     }
-    export interface ClassP2<T, P1, P2> {
+    export interface ClassP2<T, P1, P2> extends ClassBase<T> {
         new(p1: P1, p2: P2): T;
     }
-    export interface ClassP3<T, P1, P2, P3> {
+    export interface ClassP3<T, P1, P2, P3> extends ClassBase<T> {
         new(p1: P1, p2: P2, p3: P3): T;
     }
-    export interface ClassP4<T, P1, P2, P3, P4> {
+    export interface ClassP4<T, P1, P2, P3, P4> extends ClassBase<T> {
         new(p1: P1, p2: P2, p3: P3, p4: P4): T;
     }
-    export interface ClassP5<T, P1, P2, P3, P4, P5> {
+    export interface ClassP5<T, P1, P2, P3, P4, P5> extends ClassBase<T> {
         new(p1: P1, p2: P2, p3: P3, p4: P4, p5: P5): T;
     }
-    export interface ClassP6<T, P1, P2, P3, P4, P5, P6> {
+    export interface ClassP6<T, P1, P2, P3, P4, P5, P6> extends ClassBase<T> {
         new(p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6): T;
     }
-    export interface ClassP7<T, P1, P2, P3, P4, P5, P6, P7> {
+    export interface ClassP7<T, P1, P2, P3, P4, P5, P6, P7> extends ClassBase<T> {
         new(p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7): T;
     }
-    export interface ClassP8<T, P1, P2, P3, P4, P5, P6, P7, P8> {
+    export interface ClassP8<T, P1, P2, P3, P4, P5, P6, P7, P8> extends ClassBase<T> {
         new(p1: P1, p2: P2, p3: P3, p4: P4, p5: P5, p6: P6, p7: P7, p8: P8): T;
     }
     export function isSameClass(cls1: Class<object>, cls2: Class<object>) {
