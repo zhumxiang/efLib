b03ebda0baaf3e80af6c606d26e23e2546e76760
zhumxiang <zmx.jlife@gmail.com>
2024-06-25 10:35:33 +0800

    visibleInHierarchy实时计算，删除onVisibleInHierarchyChanged事件

--- src/extension/LifeCycle.ts
+++ src/extension/LifeCycle.ts
@@ -7,13 +7,10 @@ declare namespace egret {
         onExit(): void;
         /** 全局visible */
         readonly visibleInHierarchy: boolean;
-        onVisibleInHierarchyChanged(): void;
     }
 }
 namespace eflib {
     const $onAddToStage = egret.DisplayObject.prototype.$onAddToStage;
-    const $onRemoveFromStage = egret.DisplayObject.prototype.$onRemoveFromStage;
-    const $setVisible = egret.DisplayObject.prototype.$setVisible;
     class DisplayObjectEx extends egret.DisplayObject {
         parent: egret.DisplayObjectContainer & DisplayObjectEx;
         $children: DisplayObjectEx[];
@@ -21,48 +18,21 @@ namespace eflib {
         onExit() { }
         $onAddToStage(stage: egret.Stage, nestLevel: number) {
             $onAddToStage.call(this, stage, nestLevel);
-            updateVisible(this, void 0, false);
             this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onEnter, this);
             this.addEventListener(egret.Event.REMOVED_FROM_STAGE, this.onExit, this);
         }
-        $onRemoveFromStage() {
-            $onRemoveFromStage.call(this);
-            this._visibleInHierarchy = void 0;
-        }
-        _visibleInHierarchy: boolean;
         get visibleInHierarchy() {
-            return this._visibleInHierarchy;
-        }
-        onVisibleInHierarchyChanged() { }
-        $setVisible(value: boolean) {
-            if (this.$visible == value) {
-                return;
+            if (!this.stage) {
+                return false;
             }
-            $setVisible.call(this, value);
-            if (this.stage) {
-                updateVisible(this, void 0, true);
-            }
-        }
-    }
-    function updateVisible(obj: DisplayObjectEx, parentVal: boolean, recursion: boolean) {
-        if (parentVal === void 0) {
-            if (!obj.parent || obj.parent._visibleInHierarchy === void 0) {
-                parentVal = true;
-            } else {
-                parentVal = obj.parent._visibleInHierarchy;
-            }
-        }
-        let newValue = parentVal && obj.visible;
-        if (obj._visibleInHierarchy != newValue) {
-            obj._visibleInHierarchy = newValue;
-            obj.onVisibleInHierarchyChanged();
-            if (recursion) {
-                if (obj.$children) {
-                    for (let child of obj.$children) {
-                        updateVisible(child, newValue, true);
-                    }
+            let target: egret.DisplayObject = this;
+            while (target) {
+                if (!target.visible) {
+                    return false;
                 }
+                target = target.parent;
             }
+            return true;
         }
     }
     eflib.extension(DisplayObjectEx, egret.DisplayObject);
